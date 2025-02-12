db8cb074af41ca42991556875cbd4e6571f64dfa
danielgfeitosa <100307034+danielgfeitosa@users.noreply.github.com>
2025-02-12 18:56:08 -0300

    Update api.js

--- api.js
+++ api.js
@@ -1,11 +1,141 @@
-let rp = require('request-promise')
-function main(params) {
-const options = {
-    uri: "https://en.wikipedia.org/api/rest_v1/page/summary/"+ encodeURIComponent(params.object_of_interest)+ "?redirect=true",
-    json: true
+{
+
+    "message": "Seat Map Data Successfully Retrieved",
+
+    "pnrDisplayPnrResponse": {
+
+        "passengerDetails": [
+
+            {
+
+                "firstName": "DAMIEN",
+
+                "lastName": "DICKINSON",
+
+                "seats": {
+
+                    "preReservedSeats": {
+
+                        "preReservedSeat": [
+
+                            {
+
+                                "seatNumber": "1B",
+
+                                "smokingPrefOfferedIndicator": false,
+
+                                "seatTypeCode": "LMIPC",
+
+                                "seatStatusCode": "HK",
+
+                                "boardPoint": "GRU",
+
+                                "offPoint": "BSB",
+
+                                "id": "149"
+
+                            }
+
+                        ]
+
+                    },
+
+                    "seatSpecialRequests": {}
+
+                },
+
+                "nameNumber": "01.01"
+
+            }
+
+        ],
+
+        "segmentDetails": {
+
+            "segmentId": "1",
+
+            "equipmentTypeName": "Boeing 737-800"
+
+        }
+
+    },
+
+    "seatMapResponse": {
+
+        "columnsInfo": [
+
+            {
+
+                "id": "col1",
+
+                "name": "A",
+
+                "customCharacteristic": ["WINDOW"],
+
+                "characteristic": {
+
+                    "location": "WINDOW",
+
+                    "value": "W"
+
+                }
+
+            },
+
+            {
+
+                "id": "col2",
+
+                "name": "B",
+
+                "characteristic": {}
+
+            },
+
+            {
+
+                "id": "col3",
+
+                "name": "C",
+
+                "customCharacteristic": ["AISLE"],
+
+                "characteristic": {
+
+                    "location": "AISLE",
+
+                    "value": "A"
+
+                }
+
+            }
+
+        ],
+
+        "availableSeats": [
+
+            {
+
+                "position": "FRONT",
+
+                "seats": [
+
+                    ["1A", "1B", "1C"],
+
+                    ["2A", "2B", "2C"],
+
+                    ["3A", "3B", "3C"],
+
+                    ["4A", "4B", "4C"]
+
+                ]
+
+            }
+
+        ]
+
+    }
+
 }
-return rp(options)
-.then(res => {
-    return { extract : res }
-})
-}
+
+ 
